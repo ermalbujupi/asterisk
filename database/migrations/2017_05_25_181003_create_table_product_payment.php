2ad9c8cd3c09afb35dd67068f086840cd4a419e1
DESKTOP-75T2PVJ\Fidan <bicajfidan@gmail.com>
2017-05-25 20:19:50 +0200

    created migrations

---
+++ database/migrations/2017_05_25_181003_create_table_product_payment.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableProductPayment extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::create('product_payment',function(Blueprint $table){
+           $table->increments('id');
+           $table->integer('product_id');
+           $table->integer('payment_id');
+
+       });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('product_payment');
+    }
+}
--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -14,6 +14,7 @@ class ProductSeeder extends Seeder
             'brand_id'=>'1',
             'category_id'=>'1',
             'price'=>'450',
+            'price_sold'=>'500',
             'quantity'=>'1',
             'system_deleted'=>'0',
         ]);
@@ -22,6 +23,7 @@ class ProductSeeder extends Seeder
             'brand_id'=>'1',
             'category_id'=>'1',
             'price'=>'550',
+            'price_sold'=>'600',
             'quantity'=>'1',
             'system_deleted'=>'0',
         ]);
@@ -30,6 +32,7 @@ class ProductSeeder extends Seeder
             'brand_id'=>'3',
             'category_id'=>'1',
             'price'=>'420',
+            'price_sold'=>'480',
             'quantity'=>'1',
             'system_deleted'=>'0',
         ]);
@@ -38,6 +41,7 @@ class ProductSeeder extends Seeder
             'brand_id'=>'1',
             'category_id'=>'2',
             'price'=>'420',
+            'price_sold'=>'500',
             'quantity'=>'1',
             'system_deleted'=>'0',
         ]);
@@ -46,6 +50,7 @@ class ProductSeeder extends Seeder
             'brand_id'=>'1',
             'category_id'=>'1',
             'price'=>'250',
+            'price_sold'=>'300',
             'quantity'=>'1',
             'system_deleted'=>'0',
         ]);
@@ -54,6 +59,7 @@ class ProductSeeder extends Seeder
             'brand_id'=>'2',
             'category_id'=>'1',
             'price'=>'200',
+            'price_sold'=>'250',
             'quantity'=>'1',
             'system_deleted'=>'0',
         ]);
@@ -62,6 +68,7 @@ class ProductSeeder extends Seeder
             'brand_id'=>'2',
             'category_id'=>'3',
             'price'=>'15',
+            'price_sold'=>'20',
             'quantity'=>'2',
             'system_deleted'=>'0',
         ]);
@@ -70,6 +77,7 @@ class ProductSeeder extends Seeder
             'brand_id'=>'1',
             'category_id'=>'3',
             'price'=>'20',
+            'price_sold'=>'30',
             'quantity'=>'3',
             'system_deleted'=>'0',
         ]);
@@ -78,6 +86,7 @@ class ProductSeeder extends Seeder
             'brand_id'=>'1',
             'category_id'=>'3',
             'price'=>'20',
+            'price_sold'=>'30',
             'quantity'=>'4',
             'system_deleted'=>'0',
         ]);
