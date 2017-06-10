a87e99dd3a14494b0b420b37c77ce85e55a061a4
ermalbujupi <ermal.bj@gmail.com>
2017-06-10 12:49:50 +0200

    no message

--- resources/views/emails/password_reset.blade.php
+++ resources/views/emails/password_reset.blade.php
@@ -1,7 +1,7 @@
 Hi !
 <br>
 Click on the link below to change password: <br>
-<a href="<?= strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,4)).'://'.$_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'].'/password/reset/token='.$token ?>">Click here to reset password</a>
+<a href="<?= strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,4)).'://'.$_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'].'/password/reset/'.$token ?>">Click here to reset password</a>
 <br>
 <br>
 Best Regards,<br>
